6d3e9a0e34fd8ce2bf287a6091a9e51288747a07
Diego Alves <diego.alves@chiper.co>
2024-11-02 15:05:58 -0300

    Updates

---
+++ _app/immutable/nodes/2.Bz0vsDq1.js
@@ -0,0 +1,1 @@
+import{t as n,a as o}from"../chunks/disclose-version.DRjFiyor.js";import{y as e}from"../chunks/runtime.Dj5oJKmS.js";function p(t){e();var a=n("test");o(t,a)}function m(t){p(t)}export{m as component};

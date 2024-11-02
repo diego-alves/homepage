bc341af542e758763bf84a2224fcbef69221910a
Diego Alves <diego.alves@chiper.co>
2024-11-02 15:05:35 -0300

    feat: particles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
 	let { children } = $props();
+
+	let y: number;
+	let innerHeight = 0;
+	let innerWidth = 0;
+
+	function goTop() {
+		document.body.scrollIntoView();
+	}
 </script>
 
-{@render children()}
+<div class="relative mx-auto flex min-h-screen w-full max-w-[1400px] flex-col text-sm sm:text-base">
+	<div
+		class={'fixed bottom-0 z-[10] flex w-full p-10 duration-200 ' +
+			(y > 0 ? ' opacity-full pointer-events-auto' : ' pointer-events-none opacity-0')}
+	>
+		<button aria-label="up">
+			<i class="fa-solid fa-arrow-up"></i>
+		</button>
+	</div>
+	<Header />
+	{@render children()}
+	<Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
